5eb2e7003f706d0522e4898272fd96d37de16659
Julian <julianete4@gmail.com>
2023-04-04 10:52:10 -0300

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import userRouter from './users.js'
 
-var router = express.Router();
+let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
