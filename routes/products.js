16d544f869af0d1ce2de559bdce8d4e5bbda6dfc
Julian <julianete4@gmail.com>
2023-04-05 11:07:27 -0300

    destroys editados

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,12 @@
 import express from "express"
 import schema from "../schemas/products.js"
-import ProductController from "../controllers/Products/create.js"
-import UpdateControler from "../controllers/Products/Update.js"
-import getAllController from "../controllers/Products/get_all.js"
-import getOneController from "../controllers/Products/get_one.js"
-import destroyController from "../controllers/Products/destroy.js"
-import alreadyExists from "../middlewares/Products/alreadyExists.js"
+import ProductController from "../controllers/products/create.js"
+import UpdateControler from "../controllers/products/Update.js"
+import getAllController from "../controllers/products/get_all.js"
+import getOneController from "../controllers/products/get_one.js"
+import destroyController from "../controllers/products/destroy.js"
+import destroyAllController from '../controllers/products/destroy_all.js'
+import alreadyExists from "../middlewares/products/alreadyExists.js"
 import is_activeMe from "../middlewares/shops/is_activeMe.js"
 import validator from "../middlewares/validator.js"
 import passport from '../middlewares/passport.js'
@@ -13,6 +14,7 @@ import passport from '../middlewares/passport.js'
 const { create } = ProductController
 const { update } = UpdateControler
 const { destroy } = destroyController
+const { destroy_all } = destroyAllController
 
 let router = express.Router();
 
@@ -20,6 +22,7 @@ router.post('/create', passport.authenticate('jwt',{session:false}), alreadyExis
 router.put("/update/:id", passport.authenticate('jwt',{session:false}), validator(schema), is_activeMe, update)
 router.get("/", passport.authenticate('jwt',{session:false}), is_activeMe, getAllController )
 router.get('/:id', passport.authenticate("jwt", { session:false }), is_activeMe, getOneController)
+router.delete('/delete-all', passport.authenticate("jwt", { session:false }), is_activeMe, destroy_all)
 router.delete("/delete/:id", passport.authenticate("jwt", { session:false }), is_activeMe, destroy)
 
 
