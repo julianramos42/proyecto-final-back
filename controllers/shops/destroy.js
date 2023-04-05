790b5b5b8008a469b672463df716d75b9b111ab8
Julián Ramos <99379303+julianramos42@users.noreply.github.com>
2023-04-05 11:09:12 -0300

    Main extra (#7)
    * fix
    
    * fix
    
    * cambios
    
    * destroys editados

--- controllers/shops/destroy.js
+++ controllers/shops/destroy.js
@@ -1,11 +1,12 @@
 import Shop from "../../models/Shop.js";
+import Product from "../../models/Product.js"
 
 const controller = {
     destroy: async (req,res,next) => {
         try{
             let shop = await Shop.findOneAndDelete({ user_id: req.user._id })
             if(shop){
-                // ACA SE TENDRIAN QUE BORRAR TODOS LOS PRODUCTOS Y DATOS DE ESA TIENDA
+                await Product.deleteMany({store_id: shop._id})
                 return res.status(200).json({
                     success: true,
                     message: 'Shop deleted successfully'
