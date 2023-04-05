1574edacfb388c03f76566136dbb9d814f438d7c
Julian <julianete4@gmail.com>
2023-04-04 22:09:23 -0300

    fix

--- schemas/sign_in.js
+++ schemas/sign_in.js
@@ -3,20 +3,18 @@ import Joi from "joi-oid";
 const schema_signin = Joi.object({
   email: Joi.string()
     .required()
-    .min(8)
     .email({ minDomainSegments: 2 })
     .messages({
-    invalid: "Not an objets",
+      'string.empty': 'The email cannot be empty',
+      'any.required': 'A email is required',
     }),
   password: Joi
     .string()
     .required()
-    .min(8)
-    .max(50)
     .messages({
-    "string.min": "The name must have at least 8 characteres",
-    "string.max": "The name must have a maximum of 50 characteres", 
-  })
+      'string.empty': 'The password cannot be empty',
+      'any.required': 'A password is required',
+    })
 });
 
 export default schema_signin;
