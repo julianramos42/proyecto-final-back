043c1c3c81d37ab1e6891ec1881c8a5d9ad0bc99
Franco <fcalvo33@gmail.com>
2023-04-10 14:12:01 -0300

    is_seller al crear un shop

--- controllers/shops/create.js
+++ controllers/shops/create.js
@@ -1,21 +1,23 @@
-import Shop from '../../models/Shop.js'
+import Shop from "../../models/Shop.js";
+import User from "../../models/User.js";
 
 const controller = {
-    create: async(req,res,next) => {
-        const { user } = req
-        req.body.user_id = user._id
-        req.body.active = true
-        try{
-            await Shop.create(req.body)
-            return res.status(201).json({
-                success: true,
-                message: 'New shop created succesfully',
-                data: req.body
-            })
-        }catch(error){
-            next(error)
-        }
+  create: async (req, res, next) => {
+    const { user } = req;
+    req.body.user_id = user._id;
+    req.body.active = true;
+    try {
+      await Shop.create(req.body);
+      await User.findOneAndUpdate({ _id: user._id }, { is_seller: true });
+      return res.status(201).json({
+        success: true,
+        message: "New shop created succesfully",
+        data: req.body,
+      });
+    } catch (error) {
+      next(error);
     }
-}
+  },
+};
 
-export default controller
+export default controller;
