0b8ae01d9691cfc89a10befa2c0dd539d3d4ab28
Julián Ramos <99379303+julianramos42@users.noreply.github.com>
2023-04-11 16:06:19 -0300

    Main extra (#15)
    * fix
    
    * fix
    
    * cambios
    
    * destroys editados
    
    * get products modificados
    
    * cambio en la ruta
    
    * cart terminado
    
    * favourites arreglado

---
+++ controllers/favouritesController/favouritesController.js
@@ -0,0 +1,68 @@
+import Favourite from "../../models/Favourites.js"
+
+const controller = {
+  getFavourites: async (req, res) => {
+    try {
+      const user_id = req.user._id;
+      const favourites = await Favourite.find({ user_id }).populate('store_id');
+      return res.status(200).json({
+        success: true,
+        favourites
+      });
+    } catch (error) {
+      return res.status(404).json(
+        {
+          success: false,
+          message: 'Favourites not found'
+        });
+    }
+  },
+
+  addFavourites: async (req, res, next) => {
+    try {
+      req.body.user_id = req.user.id
+      req.body.store_id = req.params.shopid
+      const favourites = await Favourite.find({ store_id: req.params.shopid }).populate('store_id');
+      if (!favourites.length) {
+        await Favourite.create(req.body);
+        return res.status(201).json({
+          success: true,
+          message: 'Shop added to favourites'
+        })
+      } else {
+        return res.status(400).json({
+          success: false,
+          message: 'Shop already on favourites'
+        })
+      }
+    } catch (error) {
+      next(error)
+    }
+  },
+
+  deleteFavourite: async (req, res, next) => {
+    try {
+      await Favourite.findByIdAndDelete({ _id: req.params.id });
+      return res.status(200).json({
+        success: true,
+        message: 'Shop removed from favourites'
+      })
+    } catch (error) {
+      next(error)
+    }
+  },
+
+  deleteAll: async(req,res,next) => {
+    try{
+      await Favourite.deleteMany({user_id: req.user.id})
+      return res.status(200).json({
+        success: true,
+        message: 'Favourites clear'
+      })
+    }catch(err){
+      next(err)
+    }
+  }
+}
+
+export default controller
---
+++ routes/favourites.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import favouritesController from "../controllers/favouritesController/favouritesController.js"
+import passport from '../middlewares/passport.js'
+
+const {getFavourites, addFavourites, deleteFavourite, deleteAll} = favouritesController
+
+let router = express.Router();
+
+router.get('/', passport.authenticate('jwt',{session:false}), getFavourites);
+router.post('/:shopid', passport.authenticate('jwt',{session:false}), addFavourites);
+router.delete('/', passport.authenticate('jwt',{session:false}), deleteAll);
+router.delete('/:id', passport.authenticate('jwt',{session:false}), deleteFavourite);
+
+export default router
