05d80cc841c5d6856b5ded254bf20d1685547953
Julian <julianete4@gmail.com>
2023-04-13 12:59:37 -0300

    se agrega el token

--- controllers/favouritesController/favouritesController.js
+++ controllers/favouritesController/favouritesController.js
@@ -23,6 +23,7 @@ const controller = {
       req.body.store_id = req.params.shopid;
       const favourites = await Favourite.find({
         store_id: req.params.shopid,
+        user_id: req.user._id
       }).populate("store_id");
       if (!favourites.length) {
         await Favourite.create(req.body);
@@ -43,7 +44,7 @@ const controller = {
 
   deleteFavourite: async (req, res, next) => {
     try {
-      await Favourite.findByIdAndDelete({
+      await Favourite.findOneAndDelete({
         store_id: req.params.shopid,
         user_id: req.user._id,
       });
