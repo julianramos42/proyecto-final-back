9155d7e7796987a9254c1ae35696698e7798804b
Franco Calvo <106486606+Franco-Calvo@users.noreply.github.com>
2023-04-13 13:55:40 -0300

    Franco (#19)
    * is_seller al crear un shop
    
    * Favourite modificación
    
    * Filtros para categorías

--- controllers/shops/get_all.js
+++ controllers/shops/get_all.js
@@ -1,19 +1,30 @@
 import Shop from "../../models/Shop.js";
 
 const controller = {
-    get_all: async(req,res,next) => {
-        try{
-            let shops = await Shop.find({active: true})
-            if(shops){
-                return res.status(200).json({
-                    success: true,
-                    shops
-                })
-            }
-        }catch(err){
-            next(err)
-        }
-    }
-}
+  get_all: async (req, res, next) => {
+    try {
+      let filter = {
+        active: true,
+      };
 
-export default controller
+      if (req.query.name) {
+        filter.name = new RegExp(req.query.name.trim(), "i");
+      }
+      if (req.query.category) {
+        filter.category = req.query.category.split(",");
+      }
+
+      let shops = await Shop.find(filter);
+      if (shops) {
+        return res.status(200).json({
+          success: true,
+          shops,
+        });
+      }
+    } catch (err) {
+      next(err);
+    }
+  },
+};
+
+export default controller;
