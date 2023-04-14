885a5a1989668691c2913ad71e767a0d08014980
Julian <julianete4@gmail.com>
2023-04-14 01:20:09 -0300

    controllador admin products

--- routes/admin.js
+++ routes/admin.js
@@ -6,11 +6,15 @@ import deleteShop from '../controllers/admin/deleteShop.js'
 import getShops from '../controllers/admin/getAllShop.js'
 import getUsers from '../controllers/admin/getAllUsers.js'
 import deleteUser from '../controllers/admin/deleteUser.js'
+import getProducts from '../controllers/admin/getAllProducts.js'
+import deleteProduct from '../controllers/admin/deleteProduct.js'
 
 let router = express.Router();
 
 router.get('/users', passport.authenticate('jwt',{session:false}), is_admin, getUsers)
 router.delete('/users/delete/:userid', passport.authenticate('jwt',{session:false}), is_admin, deleteUser)
+router.get('/products', passport.authenticate('jwt',{session:false}), is_admin, getProducts)
+router.delete('/products/delete/:productid', passport.authenticate('jwt',{session:false}), is_admin, deleteProduct)
 router.get('/shops', passport.authenticate('jwt',{session:false}), is_admin, getShops)
 router.put('/shops/desactivate/:shopid', passport.authenticate('jwt',{session:false}), is_admin, desactivateShop)
 router.delete('/shops/delete/:shopid', passport.authenticate('jwt',{session:false}), is_admin, deleteShop)
