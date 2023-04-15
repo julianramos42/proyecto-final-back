b2f0e0ef147a57c2aaf9c7d8826672eae3170e87
Johan Zuluaga <johan16zulu@gmail.com>
2023-04-15 14:01:50 -0500

    implementacion correo de verificacion

--- routes/users.js
+++ routes/users.js
@@ -12,13 +12,13 @@ import passport from '../middlewares/passport.js'
 import updateController from '../controllers/auth/update.js'
 
 
-const {sign_up, sign_in, sign_out, sign_in_token} = controller
+const {sign_up, sign_in, sign_out, sign_in_token,verifyMail} = controller
 const { upd } = updateController
 
 
 let router = express.Router();
 
-
+router.get('/verify',verifyMail)
 router.post('/signup', validator(schema_signup), accountExistsSignUp, sign_up)
 router.post('/signin', validator(schema_signin), accountExistsSignIn,accountHasBeenVerified, passwordIsOk, sign_in )
 router.post('/signout', passport.authenticate('jwt',{session:false}), sign_out) 
