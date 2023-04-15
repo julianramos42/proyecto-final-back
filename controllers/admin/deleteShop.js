8e0d7ef5c762280cba62d84509b854e2d4151a0b
Julián Ramos <99379303+julianramos42@users.noreply.github.com>
2023-04-15 18:51:57 -0300

    arreglos (#27)

--- controllers/admin/deleteShop.js
+++ controllers/admin/deleteShop.js
@@ -1,17 +1,19 @@
 import Shop from '../../models/Shop.js'
 import Product from '../../models/Product.js'
 import User from '../../models/User.js'
+import Favourite from '../../models/Favourites.js'
 
 async function deleteShop(req, res, next) {
     try {
         let shop = await Shop.findOneAndDelete({ _id: req.params.shopid })
         if (shop) {
+            await Favourite.deleteMany({store_id: shop._id})
+            await Product.deleteMany({ store_id: shop._id })
             await User.findOneAndUpdate(
                 { _id: req.params.userid },
                 { is_seller: false },
                 { new: true }
             )
-            await Product.deleteMany({ store_id: shop._id })
             return res.status(200).json({
                 success: true,
                 message: 'Shop deleted'
