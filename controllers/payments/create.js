38c0f7abb9c3a71d2c299c329fab351dcd043f6d
Johan Zuluaga <121687526+JohanZ1611@users.noreply.github.com>
2023-04-15 14:58:39 -0500

    Johan (#25)
    * implementacionm filtros cruzados products
    
    * reestructuracion de grt_product para solo los de su store_id
    
    * cambio de ruta a shop
    
    * payments integrado
    
    * se agrega el token
    
    * reparacion favorites
    
    * fix
    
    * implementacion correo de verificacion
    
    * implementacion correo de pago
    
    ---------
    
    Co-authored-by: Julian <julianete4@gmail.com>

--- controllers/payments/create.js
+++ controllers/payments/create.js
@@ -1,15 +1,26 @@
 import Payment from "../../models/Payment.js";
 import Shop from '../../models/Shop.js'
+import User from '../../models/User.js'
+import createMailTransporterPay from "../../config/mailPayment.js";
 
 const controller = {
     create: async(req,res,next) => {
         try{
             let shop = await Shop.findOne({_id: req.params.shopid})
+            const user = await User.findById(req.user._id);
             req.body.store_id= shop._id
             req.body.user_id = req.user._id
             
             if(shop){
-                await Payment.create(req.body)
+                const payment = await Payment.create(req.body)
+
+                await createMailTransporterPay({
+                    email:user.email,
+                    storeId: payment.store_id,
+                    totalValue: payment.totalValue,
+                    products: payment.products
+                })
+
                 return res.status(201).json({
                     success: true,
                     message: 'Payment created'
