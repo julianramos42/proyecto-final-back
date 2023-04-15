2e84ae9c5d68a3c6b5cb8d9da1310e617efe9840
Johan Zuluaga <121687526+JohanZ1611@users.noreply.github.com>
2023-04-15 15:28:53 -0500

    Johan (#26)
    * implementacionm filtros cruzados products
    
    * reestructuracion de grt_product para solo los de su store_id
    
    * cambio de ruta a shop
    
    * payments integrado
    
    * se agrega el token
    
    * reparacion favorites
    
    * fix
    
    * implementacion correo de verificacion
    
    * implementacion correo de pago
    
    * arreglo datos para correo pago
    
    ---------
    
    Co-authored-by: Julian <julianete4@gmail.com>

--- config/mailPayment.js
+++ config/mailPayment.js
@@ -1,5 +1,5 @@
 import nodemailer from 'nodemailer'
-import User from "../models/User";
+import User from "../models/User.js";
 import Shop from "../models/Shop.js";
 
 async function createMailTransporterPay (userPayment) {
@@ -20,21 +20,21 @@ async function createMailTransporterPay (userPayment) {
 
     let productsList = "";
     products.forEach((product) => {
-      productsList += `<li>${product.name} - ${product.price}</li>`;
+      productsList += `<li>${product.title} - $${product.unit_price}</li>`;
     });
   
     // send mail with defined transport object
     let info = await transporter.sendMail({
         from: '"Lance" <LanceApp4@gmail.com>',
         to: email,
-        subject:"Verify your email...",
+        subject:"Purchase details...",
         html: `<p>Hi ${user.name},</p>
         <p>Thank you for your purchase at ${shop.name}!</p>
         <p>Here's a summary of your order:</p>
         <ul>
           ${productsList}
         </ul>
-        <p>Total: ${totalValue}</p>
+        <p>Total: $${totalValue}</p>
         <img src="${shop.photo}" alt="${shop.name}" width="200px" />
         <p>Best regards,</p>
         <p>The Lance Team</p>`
