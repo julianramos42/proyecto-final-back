0f619c7ea6eb51ad436691fdcc6472df63cbcf7e
Julián Ramos <99379303+julianramos42@users.noreply.github.com>
2023-04-16 18:02:47 -0300

    arreglos en controlladores (#29)

--- controllers/admin/deleteShop.js
+++ controllers/admin/deleteShop.js
@@ -2,11 +2,13 @@ import Shop from '../../models/Shop.js'
 import Product from '../../models/Product.js'
 import User from '../../models/User.js'
 import Favourite from '../../models/Favourites.js'
+import Category from '../../models/category.js'
 
 async function deleteShop(req, res, next) {
     try {
         let shop = await Shop.findOneAndDelete({ _id: req.params.shopid })
         if (shop) {
+            await Category.deleteMany({store_id: shop._id})
             await Favourite.deleteMany({store_id: shop._id})
             await Product.deleteMany({ store_id: shop._id })
             await User.findOneAndUpdate(
--- controllers/admin/deleteUser.js
+++ controllers/admin/deleteUser.js
@@ -1,15 +1,20 @@
 import User from '../../models/User.js'
 import Shop from '../../models/Shop.js'
 import Product from '../../models/Product.js'
+import Favourite from '../../models/Favourites.js'
+import Category from '../../models/category.js'
 
 async function deleteUser(req, res, next) {
     try {
         let user = await User.findOneAndDelete({ _id: req.params.userid })
         if (user) {
-            let store = await Shop.findOneAndDelete({user_id: user._id})
-            if(store){
-                await Product.deleteMany({store_id: store._id})
+            let store = await Shop.findOneAndDelete({ user_id: user._id })
+            if (store) {
+                await Product.deleteMany({ store_id: store._id })
+                await Category.deleteMany({store_id: store._id})
+                await Favourite.deleteMany({store_id: store._id})
             }
+            await Favourite.deleteMany({ user_id: user._id })
             return res.status(200).json({
                 success: true,
                 message: 'User deleted'
