a69002bb0da46384759a5e6846dd9e264fea8ea6
Julian <julianete4@gmail.com>
2023-04-17 11:20:21 -0300

    mail de compra modificado y arreglado

--- config/mailPayment.js
+++ config/mailPayment.js
@@ -2,20 +2,20 @@ import nodemailer from 'nodemailer'
 import User from "../models/User.js";
 import Shop from "../models/Shop.js";
 
-async function createMailTransporterPay (userPayment) {
+async function createMailTransporterPay(userPayment) {
     const { email, storeId, totalValue, products } = userPayment;
 
     const user = await User.findOne({ email });
     const shop = await Shop.findById(storeId);
-  
+
     let transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true, 
-      auth: {
-        user: process.env.EMAIL, 
-        pass: process.env.EMAIL_PASS, 
-      },
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.EMAIL_PASS,
+        },
     });
 
 
@@ -31,14 +31,14 @@ async function createMailTransporterPay (userPayment) {
 
     let productsQuantity = "";
     products.forEach((product) => {
-        productsQuantity += `<li style="font-family:'Montserrat', sans-serif;font-weight: 400;font-size: 12px;color:#495464;margin-bottom: 5px;">$${product.quantity}</li>`;
+        productsQuantity += `<li style="font-family:'Montserrat', sans-serif;font-weight: 400;font-size: 12px;color:#495464;margin-bottom: 5px;">${product.quantity}</li>`;
     });
-  
+
     // send mail with defined transport object
     let info = await transporter.sendMail({
         from: '"Lance" <LanceApp4@gmail.com>',
         to: email,
-        subject:"Purchase details...",
+        subject: "Purchase details...",
         html: `<!DOCTYPE html>
         <html>
         <head>
@@ -48,7 +48,6 @@ async function createMailTransporterPay (userPayment) {
           <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
         </head>
         <body>
-<<<<<<< HEAD
             <div style="margin:0;padding:0;background-color:#F4F4F2;">
               <table cellpadding="0" cellspacing="0" border="0" width="100%" bgcolor="#F4F4F2" style="max-width:614px;margin:0 auto;">
                 <tr>
@@ -71,9 +70,9 @@ async function createMailTransporterPay (userPayment) {
                   </td>
                 </tr>
                 <tr>
-                  <td height="487" style="font-size:0;line-height:0;border-collapse:collapse;position:relative;">
-                    <div style="position:absolute;width:433px;height:487px;left:50%;top:45%;transform:translate(-50%,-50%);overflow:hidden;background-color:white;border-radius:8px;box-shadow:0px 4px 4px rgba(0,0,0,0.25);">
-                      <div style="width:433px;height:15%;display:flex;align-items:flex-end;padding-left:44px;">
+                  <td height="400" style="font-size:0;line-height:0;border-collapse:collapse;position:relative;">
+                    <div style="position:absolute;width:50%;height:400px;padding: 50px 25% 0 25%;left:50%;top:45%;transform:translate(-50%,-50%);overflow:hidden;background-color:white;border-radius:8px;box-shadow:0px 4px 4px rgba(0,0,0,0.25);">
+                      <div style="width:433px;height:5%;display:flex;align-items:flex-end;padding-left:44px;">
                         <h2 style="font-family:'Montserrat', sans-serif;font-weight:500;font-size:16px;margin:0;">Summary of your purchase</h2>
                       </div>
                     <div style="width:433px;height:70%;display:flex;align-items:center;justify-content:center;">
@@ -107,91 +106,31 @@ async function createMailTransporterPay (userPayment) {
                             </tr>
                             <tr>
                                 <td style="font-family:'Montserrat', sans-serif;font-weight:500;font-size:14px;padding:10px;">Total:</td>
-                                <td style="font-family:'Montserrat', sans-serif;font-weight:400;font-size:14px;text-align:right;padding:10px;">$${totalValue}</td>
+                                <td style="font-family:'Montserrat', sans-serif;font-weight:400;font-size:14px;padding:10px;">
+                                    <ul>
+                                        $${totalValue}
+                                    </ul>
+                                </td>
                             </tr>
                         </table>
                     </div>
                   </td>
-                </tr>
-                <tr>
+                  <tr>
                     <td height="60" style="font-size:0;line-height:0;border-collapse:collapse;">
-                        <div style="width:100%;height:100%;display:flex;align-items:center;justify-content:center;">
+                        <div style="width:100%;height:100%;display:flex;align-items:center;justify-content:center;padding-left:25%">
                             <a style="text-decoration: none;font-family:'Montserrat', sans-serif;font-weight: 700;font-size: 16px;color: white;padding: 30px 75px;background: #161616;border-radius: 14px;" href="${process.env.CLIENT_URL}/shops">SEE MORE PRODUCTS</a>
                         </div>
                     </td>
                 </tr>
-
+                </tr>
               </table>
             </div> 
-=======
-            <div style="margin:0;padding:0;display: flex;justify-content:center;align-items: center;" width="100%">
-                <div style="width:614px;background-color:#F4F4F2;height:730px;">
-                    <div style="width:100%;background-color:#F4F4F2;height:6vh;display:flex;align-items: center;justify-content:space-between ">
-                        <img style="width:129px;height:38px;padding-left: 70px;object-fit: cover;object-position: center;" src="https://i.postimg.cc/HWFJjCKw/logo.png" alt="logo">
-                        <h2 style="font-family:'Montserrat', sans-serif;font-weight: 400;font-size: 18px;padding-right:70px ;">${user.name}</h2>
-                    </div>
-                    <div style="height:208px;width:100%;background-color:#161616;display:flex;">
-                        <div style="width:430px;height:208px">
-            
-                            <h2 style="font-family:'Montserrat', sans-serif;font-weight: 400;font-size: 16px;color: white;padding-left: 100px;padding-top: 30px;">You bought in the store ${shop.name}</h2>
-            
-                            <h2 style="font-family:'Montserrat', sans-serif;font-weight: 700;font-size: 20px;color: white;padding-left: 100px;">Arrives within 15 days</h2>
-            
-                        </div>
-                        <div style="width:184px;height:208px">
-                            <img style="width:80px;height:80px;border-radius: 100PX;margin-top:40px;margin-left: 20px;object-fit: cover;object-position: center;" src="${shop.photo}" alt="logo">
-                        </div>
-                    </div>
-                <td height="487" style="font-size:0;line-height:0;border-collapse:collapse;position:relative;">
-                    <div style="position:absolute;width:433px;height:487px;left:50%;top:52%;transform:translate(-50%,-50%);overflow:hidden;background-color: white;border-radius: 8px;box-shadow:0px 4px 4px rgba(0, 0, 0, 0.25); ;">
-            
-                        <div style="width:433px;height:15%;display: flex;align-items: flex-end;">
-                            <h2 style="font-family:'Montserrat', sans-serif;font-weight: 500;font-size: 16px;padding-left: 44px;">Summary of your purchase</h2>
-                            
-                        </div>
-                        <div style="width:433px;height:65%">
-                            <div style="width:433px;height:20%;display: flex;align-items: flex-end;">
-                                <div style="display:flex;justify-content: center;align-items: center;gap: 10px;">
-                                    <img style="width:24px;height:24px;padding-left: 44px;object-fit: cover;object-position: center;" src="https://i.postimg.cc/DydDfvWM/transport.png" alt="transport">
-                                    <h2 style="font-family:'Montserrat', sans-serif;font-weight: 400;font-size: 12px;color: #495464;text-align: center;">Home delivery</h2>
-                                </div>
-                            </div>
-                            <div style="width:433px;height:auto;display: flex;align-items: flex-start;flex-direction: column;">
-                                <div style="display:flex;justify-content: center;align-items: center;gap: 10px;">
-                                    <img style="width:24px;height:24px;padding-left: 44px;object-fit: cover;object-position: center;" src="https://i.postimg.cc/Jh4tYzJV/products.png" alt="products">
-                                    <h2 style="font-family:'Montserrat', sans-serif;font-weight: 400;font-size: 12px;color: #495464;text-align: center;">Products:</h2>
-                                </div>
-                                <div style="display:flex;justify-content: center;align-items: center;gap: 10px;padding-left: 60px;">
-                                    <ul>
-                                        ${productsList}
-                                    </ul>
-                                </div>
-                            </div>
-                            <div style="width:433px;height:20%;display: flex;align-items: flex-start;flex-direction: column;">
-                                <div style="display:flex;justify-content: center;align-items: center;gap: 10px;">
-                                    <img style="width:24px;height:24px;padding-left: 44px;object-fit: cover;object-position: center;" src="https://i.postimg.cc/q7fj2WC3/pay.png" alt="pay">
-                                    <h2 style="font-family:'Montserrat', sans-serif;font-weight: 400;font-size: 12px;color: #495464;text-align: center;">Paid $${totalValue}</h2>
-                                </div>
-                                <h2 style="font-family:'Montserrat', sans-serif;font-weight: 400;font-size: 12px;color: #495464;text-align: center;padding-left: 80px;">With credit card</h2>
-                            </div>
-                        </div>
-                        <div style="width:433px;height:20%;display: flex;justify-content: center;align-items: center;">
-                            <a style="text-decoration: none;font-family:'Montserrat', sans-serif;font-weight: 700;font-size: 16px;color: white;padding: 21px 75px;background: #161616;border-radius: 14px;" href="${process.env.CLIENT_URL}/shops">SEE MORE PRODUCTS</a>
-                        </div>
-                        
-                    </div>
-                </td>
-                    
-                </div>
-            
-            </div>
->>>>>>> e6f270348bd5d6c576ad6542c19de4c164c2b52f
         </body>
         </html>
         `
     });
-  
+
 }
-  
+
 
 export default createMailTransporterPay
